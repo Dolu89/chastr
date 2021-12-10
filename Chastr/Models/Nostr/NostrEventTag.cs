e4c0a2187bd03ec480ba9b73a7dc9aa3b35fbc70
Dolu <doludev@protonmail.com>
2021-12-10 20:36:44 +0100

    Add nostr events object

---
+++ Chastr/Models/Nostr/NostrEventTag.cs
@@ -0,0 +1,26 @@
+﻿using Chastr.Utils.JsonConverts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Chastr.Models.Nostr
+{
+    [JsonConverter(typeof(NostrEventTagJsonConverter))]
+    public class NostrEventTag
+    {
+        public string Id { get; set; }
+        public string EventId { get; set; }
+        public string TagIdentifier { get; set; }
+        public List<string> Data { get; set; }
+
+        public NostrEvent Event { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(Data.Prepend(TagIdentifier));
+        }
+    }
+}
---
+++ Chastr/Utils/JsonConverts/NostrEventTagJsonConverter.cs
@@ -0,0 +1,55 @@
+﻿using Chastr.Models.Nostr;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Chastr.Utils.JsonConverts
+{
+    public class NostrEventTagJsonConverter : JsonConverter<NostrEventTag>
+    {
+        public override NostrEventTag? Read(ref Utf8JsonReader reader, Type typeToConvert,
+            JsonSerializerOptions options)
+        {
+            var result = new NostrEventTag();
+            if (reader.TokenType != JsonTokenType.StartArray)
+            {
+                throw new JsonException("Nostr Event Tags are an array");
+            }
+
+            reader.Read();
+            var i = 0;
+            while (reader.TokenType != JsonTokenType.EndArray)
+            {
+                if (i == 0)
+                {
+                    result.TagIdentifier = reader.GetString();
+                }
+                else
+                {
+                    result.Data.Add(reader.GetString());
+                }
+
+                i++;
+            }
+
+            return result;
+        }
+
+        public override void Write(Utf8JsonWriter writer, NostrEventTag value, JsonSerializerOptions options)
+        {
+            if (value is null)
+            {
+                writer.WriteNullValue();
+                return;
+            }
+
+            writer.WriteStartArray();
+            writer.WriteStringValue(value.TagIdentifier);
+            value.Data?.ForEach(writer.WriteStringValue);
+
+            writer.WriteEndArray();
+        }
+    }
+}
